b9e6bd5d1a51be0b548595d722a6f68f66c4a040
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-05-14 16:57:41 +0900

    Update settings.py

--- django_pjt4/settings.py
+++ django_pjt4/settings.py
@@ -126,6 +126,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # media file 저장 경로
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
